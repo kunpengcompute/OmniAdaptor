163bcb7219335b98b6f85cf04e9ea88cb6112cea
IcE_test <1054919371@qq.com>
2026-02-28 14:15:38 +0800

    修改sp恢复逻辑，key/value按照base64传递到Omni后解析

--- omnistream/omniop-flink-extension/java/src/main/java/com/huawei/omniruntime/flink/runtime/restore/KeyGroupEntry.java
+++ omnistream/omniop-flink-extension/java/src/main/java/com/huawei/omniruntime/flink/runtime/restore/KeyGroupEntry.java
@@ -4,35 +4,9 @@
 
 package com.huawei.omniruntime.flink.runtime.restore;
 
-import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.core.JsonGenerator;
-import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.JsonSerializer;
-import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.SerializerProvider;
-import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.annotation.JsonSerialize;
-
-import java.io.IOException;
-
-class ByteArrayToIntArraySerializer extends JsonSerializer<byte[]> {
-    @Override
-    public void serialize(byte[] value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
-        if (value == null) {
-            gen.writeNull();
-            return;
-        }
-        int[] intArray = new int[value.length];
-        for (int i = 0; i < value.length; i++) {
-            intArray[i] = value[i];
-        }
-        gen.writeArray(intArray, 0, intArray.length);
-    }
-}
-
 public class KeyGroupEntry {
     private final int kvStateId;
-
-    @JsonSerialize(using = ByteArrayToIntArraySerializer.class)
     private final byte[] key;
-
-    @JsonSerialize(using = ByteArrayToIntArraySerializer.class)
     private final byte[] value;
 
     public KeyGroupEntry(int kvStateId, byte[] key, byte[] value) {

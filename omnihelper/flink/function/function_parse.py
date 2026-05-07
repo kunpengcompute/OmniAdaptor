ebf2b7d4858c3375e37ff639d4058feaf3401930
zjw_david <13041298066@163.com>
2026-05-07 11:18:47 +0800

    解析算子函数

---
+++ omnihelper/flink/function/function_parse.py
@@ -0,0 +1,16 @@
+"""
+   Copyright (c) Huawei Technologies Co., Ltd. 2026. All rights reserved.
+   You can use this software according to the terms and conditions of the Mulan PSL v2.
+   You may obtain a copy of Mulan PSL v2 at:
+            http://license.coscl.org.cn/MulanPSL2
+   THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+   EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+   MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+   See the Mulan PSL v2 for more details.
+
+函数解析模块 - 已整合到 parse.py
+此文件保留用于向后兼容，请使用 parse.py 中的 FlinkFunctionParser 类
+"""
+from omnihelper.flink.parse import FlinkFunctionParser
+
+__all__ = ['FlinkFunctionParser']
